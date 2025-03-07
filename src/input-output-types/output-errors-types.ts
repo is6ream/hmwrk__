65ce7701bf6992f011368acb564c37efe2abd269
is6ream <dnlils10@icloud.com>
2025-03-07 18:08:07 +0500

    Закончил работу с валидацией

--- src/input-output-types/output-errors-types.ts
+++ src/input-output-types/output-errors-types.ts
@@ -1,3 +1,7 @@
+import { BlogInputModel, PostInputModel } from "./blogsAndPost-types"
+
 export type OutputErrorsType = {
     errorsMessages: { message: string, field: string }[]
 }
+
+export type FieldNamesType = keyof BlogInputModel | keyof PostInputModel
