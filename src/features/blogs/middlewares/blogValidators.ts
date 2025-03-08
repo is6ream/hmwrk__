07c52d5e45d2c463ed8f86f709a299be6680b5d8
is6ream <dnlils10@icloud.com>
2025-03-08 07:57:47 +0500

    Подключил findBlogValidator

---
+++ src/features/blogs/middlewares/blogValidators.ts
@@ -0,0 +1,37 @@
+import { body, validationResult } from 'express-validator'
+import { inputCheckErrorsMiddleware } from '../../../global-middlewares/inputCheckErrorsMiddleware'
+import { NextFunction, Request, Response } from 'express'
+import { blogsRepository } from '../blogsRepository'
+import { adminMiddleware } from '../../../global-middlewares/admin-middleware'
+import { title } from 'process'
+
+// name: string // max 15
+// description: string // max 500
+// websiteUrl: string // max 100 ^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$
+
+export const nameValidator = body('name').isString().withMessage('not string')
+    .trim().isLength({ min: 1, max: 15 }).withMessage('more then 15 or 0')
+
+export const descriptionValidator = body('description').isString().withMessage('not string')
+    .trim().isLength({ min: 1, max: 500 }).withMessage('more then 500 or 0')
+
+export const websiteUrlValidator = body('websiteUrl').isString().withMessage('not string')
+    .trim().isURL().withMessage('not url')
+    .isLength({ min: 1, max: 100 }).withMessage('more then 100 or 0')
+
+export const findBlogValidator = (req: Request<{id: string}>, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.status(400).json({codeResult: 1, messages: errors.array()})
+    } else{
+        next()
+    }
+}
+
+export const blogValidators = [
+    adminMiddleware,
+    nameValidator,
+    descriptionValidator,
+    websiteUrlValidator,
+    inputCheckErrorsMiddleware
+]
