0bcf03239438a0191df3ebe450bc49e67200ecbe
is6ream <dnlils10@icloud.com>
2025-05-10 16:54:33 +0500

    registered blog deletion service

--- src/blogs/routes/handlers/delete-blog.handler.ts
+++ src/blogs/routes/handlers/delete-blog.handler.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express";
 import { HttpStatus } from "../../../core/types/http-statuses";
-import { errorsHandler } from "../../../core/errors/errors.handler";
-// import { blogsService}
+import { blogsService } from "../../application/dtos/dtos/blogs.service";
 
-
+const deleteBlogHandler = (async (req: Request, res: Response) => {
+    try {
+        const deleteBlog = await blogsService.delete(req.params.id)
+        res.sendStatus(HttpStatus.Created)
+    } catch (err) {
+        console.error("Error deleting blog: ", err)
+        res.send(HttpStatus.InternalServerError).json({ error: "An internal server occured" })
+    }
+}) 
