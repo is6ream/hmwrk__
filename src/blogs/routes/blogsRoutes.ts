66f13fdb57ab76ab95b7380b6bb534876387e016
is6ream <dnlils10@icloud.com>
2025-05-16 07:55:31 +0500

    подключил валидацию к роутам

--- src/blogs/routes/blogsRoutes.ts
+++ src/blogs/routes/blogsRoutes.ts
@@ -8,14 +8,33 @@ import { deleteBlogHandler } from "./handlers/delete-blog.handler";
 import { updateBlogHandler } from "./handlers/update-blog.handler";
 import { paginationAndSortingValidation } from "../../core/middlewares/validation/pagination-and-sorting-validation";
 import { BlogSortField } from "./input/blog-sort-field";
+import { inputValidationResultMiddleware } from "../../core/middlewares/validation/input-validation-result.middleware";
 
 export const blogsRouter = Router()
 
-blogsRouter.get('/blogs', paginationAndSortingValidation(BlogSortField), getAllBlogsHandler) /*суть валидации и пагинации заключается во избежание SQL-инъекций 
-и в целом повышением уровня безопасности и стабилизации приложения*/ 
-blogsRouter.post('/blogs', ...blogValidators, createBlogHandler)
-blogsRouter.get('/blogs/:id/posts', idValidation) //здесь остановился, нужно прописать валидацию
-blogsRouter.get('/blogs/:id', findBlogValidator, findBlogHandler)
-blogsRouter.delete('/blogs/:id', adminMiddleware, findBlogValidator, deleteBlogHandler)
-blogsRouter.put('/blogs/:id', findBlogValidator, ...blogValidators, updateBlogHandler)
+blogsRouter
+    .get(
+        '/blogs',
+        paginationAndSortingValidation(BlogSortField), inputValidationResultMiddleware, getAllBlogsHandler
+    ) /*суть валидации и пагинации заключается во избежание SQL-инъекций 
+и в целом повышением уровня безопасности и стабилизации приложения*/
+blogsRouter.post(
+    '/blogs',
+    ...blogValidators, createBlogHandler
+)
+blogsRouter.get(
+    '/blogs/:id/posts', idValidation
+) //здесь остановился, нужно прописать валидацию
+
+blogsRouter.get(
+    '/blogs/:id',idValidation, inputValidationResultMiddleware,
+    findBlogValidator, findBlogHandler
+)
+blogsRouter.delete(
+    '/blogs/:id', adminMiddleware, findBlogValidator, deleteBlogHandler
+)
+blogsRouter.put(
+    '/blogs/:id',
+    findBlogValidator, ...blogValidators, updateBlogHandler
+)
 
--- src/core/middlewares/validation/input-validation-result.middleware.ts
+++ src/core/middlewares/validation/input-validation-result.middleware.ts
@@ -3,18 +3,45 @@ import { NextFunction, Request, Response } from "express";
 import { HttpStatus } from "../../types/http-statuses";
 import { ValidationErrorListOutput } from "../../types/validationError.dto";
 import { ValidationErrorType } from "../../types/validationError";
+import { error } from "console";
 
 export const createErrorMessages = (
-    errors: ValidationErrorType[],
+    errors: ValidationErrorType[]
 ): ValidationErrorListOutput => {
     return {
         errors: errors.map((error) => ({
             status: error.status,
             detail: error.detail,
-            source: { pointer: error.source ?? ''},
-            code: error.code ?? null,
-        })),
+            source: { pointer: error.source ?? '' },
+            code: error.code ?? null
+        }))
     }
 }
 
-//здесь остановился
+const formaValidationError = (error: ValidationError): ValidationErrorType => {
+    const expressError = error as unknown as FieldValidationError;
+
+    return {
+        status: HttpStatus.BadRequest,
+        source: expressError.path,
+        detail: expressError.msg
+    }
+}
+
+export const inputValidationResultMiddleware = (
+    req: Request<{}, {}, {}, {}>,
+    res: Response,
+    next: NextFunction
+) => {
+    const errors = validationResult(req)
+        .formatWith(formaValidationError)
+        .array({ onlyFirstError: true })
+
+    if (errors.length > 0) {
+        res.status(HttpStatus.BadRequest).json(createErrorMessages(errors));
+        return
+    }
+    next()
+}//остановился на разборе данного middleware
+
+
